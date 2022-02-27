cb7122895b056c62337341f705a1baa5dca56ce4
TinaRenate <100517450+TinaRenate@users.noreply.github.com>
2022-02-27 14:08:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿//programa lai atrastu kopīgo lielāko iespējamo dalītāju
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // lietotājs ievada 1.skaitli
+      Console.WriteLine("Ievadi skaitli:");
+
+      // saglabā skaitli mainīgajā lielumā - int
+      int x = Convert.ToInt32(Console.ReadLine());
+
+      // lietotājs ievada 2. skaitli
+      Console.WriteLine("Ievadi skaitli:");
+
+      // saglabā skaitli mainīgajā lielumā - int
+      int y = Convert.ToInt32(Console.ReadLine());
+
+      Console.Write("Lielākais kopīgais dalītājs " + x + " un " + y + " ir: ");
+      Console.WriteLine(GCD(x, y));
+    }
+    private static int GCD(int x, int y)
+    {
+        if(y == 0)
+            return x;
+        else
+            return GCD(y, x % y);
+    }
+  }
+}
